c55d2e4a0bf05f496b31ac5a0dc7478aacf0a1bc
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-09-21 21:22:10 +0300

    added ExceptionMiddleware

--- Core/Extensions/ExceptionMiddleware.cs
+++ Core/Extensions/ExceptionMiddleware.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
 
 namespace Core.Extensions
@@ -36,18 +37,34 @@ namespace Core.Extensions
 
             string message = "Internal Server Error";
 
+            IEnumerable<ValidationFailure> errors;
+
             // eğer aldığımız hata validation exception ise bu mesajı değiştir
             if (e.GetType() == typeof(ValidationException))
             {
                 message = e.Message;
+                errors = ((ValidationException)e).Errors;
+                // sisteme yazıyoruz (aşağıda objeye yazıyoruz), buraya yazmamız lazım
+                httpContext.Response.StatusCode = 400;
+
+                // message kısmını yazmayabiliriz
+                // sistem ile ilgili bilgi vermememiz lazım
+                return httpContext.Response.WriteAsync(new ValidationErrorDetails
+                {
+                    StatusCode = 400,
+                    Message = message,
+                    ValidationErrors = errors
+                }.ToString());
+
             }
 
             // vereceğimiz response'u ErrorDetails formatında döndür
-            return httpContext.Response.WriteAsync(new ErrorDetails
-            {
-                StatusCode = httpContext.Response.StatusCode,
-                Message = message
-            }.ToString());
+            // sistemsel bir yapı, veritabanım çalışmadığında mesela bu hatayı döndürür
+                return httpContext.Response.WriteAsync(new ErrorDetails
+                {
+                    StatusCode = httpContext.Response.StatusCode,
+                    Message = message
+                }.ToString());
+            }
         }
-    }
 }
