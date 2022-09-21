ba4a6c6db697568484ea8654c5583aa6aee7e629
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-09-21 17:27:52 +0300

    Added ExceptionMiddleware

---
+++ Core/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+
+namespace Core.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureCustomExceptionMiddleware(this IApplicationBuilder app)
+        {
+            // asp.net core yazmış zaten
+            // biz sadece o yaşam döngüsünde hangi middleware'ımızı eklemek istiyorsak böyle yazıyoruz
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
